c9ea120d3271ba8fe67f813c81aef7a506dc5e2c
Andy McCall <andymc903@gmail.com>
2020-02-10 08:22:06 -0800

    added some stuff to initialize maker

--- index.js
+++ index.js
@@ -2,11 +2,28 @@
 const jsyaml = require('js-yaml');
 const eRoR = require('./expected-value');
 const fs = require('fs');
+const Maker = require('@makerdao/dai');
+const McdPlugin = require('@makerdao/dai-plugin-mcd').default;
+const {ETH, BAT} = require('@makerdao/dai-plugin-mcd');
 
 async function main() {
   const constants = jsyaml.load(fs.readFileSync('./constants.yml', 'utf8'));
   console.log(constants);
   eRoR.setRiskFree(constants.risk_free_rate);
+  const mcdOptions = {
+    cdpTypes: [
+      {currency: ETH, ilk: "ETH_A"},
+      {currency: BAT, ilk: "BAT_A"}
+    ]
+  };
+  const maker = await Maker.create("http", {
+    url: process.env.KOVAN_URL,
+    plugins: [
+      [McdPlugin, mcdOptions]
+    ]
+  });
+  const manager = maker.service('mcd:cdpManager');
+  console.log("maker initialized");
 }
 
 (async() => {
