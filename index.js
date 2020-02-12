2bedc69a5ebeb99b2b267794d691df4806cf38e3
Andy McCall <andymc903@gmail.com>
2020-02-11 20:01:30 -0800

    it kind of works

--- index.js
+++ index.js
@@ -5,6 +5,14 @@ const fs = require('fs');
 const Maker = require('@makerdao/dai');
 const McdPlugin = require('@makerdao/dai-plugin-mcd').default;
 const {ETH, BAT} = require('@makerdao/dai-plugin-mcd');
+const addr = require('./addresses');
+const api = require('etherscan-api').init(process.env.ETHERSCAN_KEY);
+
+async function asyncForEach(array, callback) {
+  for (let index = 0; index < array.length; index++) {
+    await callback(array[index], index, array);
+  }
+}
 
 async function main() {
   const constants = jsyaml.load(fs.readFileSync('./constants.yml', 'utf8'));
@@ -23,7 +31,58 @@ async function main() {
     ]
   });
   const manager = maker.service('mcd:cdpManager');
-  console.log("maker initialized");
+  const block  = await maker.service('web3').blockNumber();
+  console.log("Getting proxies created before block", block);
+  // Get the transaction list for the PROXY_REGISTRY contract from etherscan
+  let pageNumber = 1;
+  const startBlock = 1;
+  const offset = 100;
+  const sort = "asc";
+  let txlist = await api.account.txlist(
+    addr.PROXY_REGISTRY,
+    startBlock,
+    block,
+    pageNumber,
+    offset,
+    sort
+  );
+
+  while (txlist.result.length > 0 ) {
+    let proxyPromises =  [];
+
+    for (let i = 0; i < txlist.result.length; i++) {
+      proxyPromises.push(maker.service('proxy').getProxyAddress(txlist.result[i].from));
+    }
+
+    console.log("waiting on proxy addresses. page: ", pageNumber);
+    let proxyAddresses = await Promise.all(proxyPromises);
+    proxyAddresses = proxyAddresses.filter(e => e != null);
+    await asyncForEach(proxyAddresses, async addr => {
+      console.log("getting cdps", addr);
+      const data = await manager.getCdpIds(addr);
+      await asyncForEach(data, async (cdp) => {
+        const vault = await manager.getCdp(cdp.id);
+        console.log([
+          vault.collateralAmount,
+          vault.collateralValue, // value in USD given current price feed values
+          vault.debtValue,
+          vault.collateralizationRatio,
+          vault.liquidationPrice
+        ].map(x => x.toString()));
+      })
+    });
+
+    proxyPromises = [];
+    pageNumber++;
+    txlist = await api.account.txlist(
+      addr.PROXY_REGISTRY,
+      startBlock,
+      block,
+      pageNumber,
+      offset,
+      sort
+    )
+  }
 }
 
 (async() => {
