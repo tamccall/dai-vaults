9449ef7aeeccc1a83bb59d4fe4ef5d4fe54397a3
Andy McCall <andymc903@gmail.com>
2020-02-12 23:30:05 -0800

    Added calc.js

---
+++ calc.js
@@ -0,0 +1,15 @@
+async function main() {
+  // TODO: implement this.
+}
+
+(async() => {
+  console.log('starting...');
+
+  await main().catch(e => {
+    console.error("error in main\n", e);
+    process.exit(1);
+  });
+
+  console.log('done...');
+  process.exit(0);
+})();
